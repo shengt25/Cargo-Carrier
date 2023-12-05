83f663975abbb7647c3d69a71b6da421ac48a91e
Sheng.T <alxt9@outlook.com>
2023-12-05 16:56:35 +0200

    database file

--- game.py
+++ game.py
@@ -1,7 +1,6 @@
 from Player import Player
 from Plane import Plane
-from AirportManager import AirportManager
-from utils import Database
+from utils import Database, gen_airports
 from Shop import Shop
 
 
@@ -9,9 +8,9 @@ class Game:
     def __init__(self, game_id, database_config, item_list, debug=False):
         self.game_id = game_id
         self.database = Database(database_config)
-
         self.player = Player(database=self.database, game_id=self.game_id, name="st", money=100000, fuel=100000,
                              time=800)
-        self.airport_manager = AirportManager(self.database, self.player)
-        self.shop = Shop(self.database, self.player, item_list)
-
+        self.shop = Shop(database=self.database, player=self.player, items=item_list)
+        airports = gen_airports(database=self.database, player=self.player, number=10)
+        self.plane = Plane(player=self.player, database=self.database, airports=airports,
+                           fuel_per_km=0.1, speed_per_h=1.2, emission_per_km=1.2, reward_per_km=2, hire_cost=600)
