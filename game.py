6e1118741da4752ea8da5a9c495f97f3cc88df37
Sheng.T <alxt9@outlook.com>
2023-12-05 23:32:26 +0200

    use actual param

--- game.py
+++ game.py
@@ -5,12 +5,17 @@ from Shop import Shop
 
 
 class Game:
-    def __init__(self, game_id, database_config, item_list, debug=False):
+    def __init__(self, game_id, database_param, shop_param, player_name, airport_number, player_param, plane_param,
+                 verbose=False):
         self.game_id = game_id
-        self.database = Database(database_config)
-        self.player = Player(database=self.database, game_id=self.game_id, name="st", money=100000, fuel=100000,
-                             time=800)
-        self.shop = Shop(database=self.database, player=self.player, items=item_list)
-        airports = gen_airports(database=self.database, player=self.player, number=10)
+        self.database = Database(database_param)
+        self.player = Player(database=self.database, game_id=self.game_id, name=player_name,
+                             money=player_param["money"], fuel=player_param["fuel"], time=player_param["time"])
+        self.shop = Shop(database=self.database, player=self.player, items=shop_param)
+        airports = gen_airports(database=self.database, player=self.player, number=airport_number)
         self.plane = Plane(player=self.player, database=self.database, airports=airports,
-                           fuel_per_km=0.1, speed_per_h=1.2, emission_per_km=1.2, reward_per_km=2, hire_cost=600)
+                           fuel_per_km=plane_param["fuel_per_km"],
+                           speed_per_h=plane_param["speed_per_h"],
+                           emission_per_km=plane_param["emission_per_km"],
+                           reward_per_km=plane_param["reward_per_km"],
+                           hire_cost=plane_param["hire_cost"], )
