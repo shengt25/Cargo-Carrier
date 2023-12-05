8b8361bd81ae1d70ac381ec1d7a1f57b13d51f73
Sheng.T <alxt9@outlook.com>
2023-12-05 23:19:05 +0200

    update route and response

--- server.py
+++ server.py
@@ -2,97 +2,191 @@ from flask import Flask, request, redirect, render_template
 import hashlib
 import time
 from Game import Game
+from datetime import datetime
 
 app = Flask(__name__)
-
-
-class GameManager:
-    game_list = {}
 
 
 def generate_game_id():
     timestamp = str(time.time())
     hash_obj = hashlib.sha256(timestamp.encode())
     hash_string = hash_obj.hexdigest()
-    temp_id = hash_string[:40]
-    return temp_id
+    game_id = hash_string[:40]
+    return game_id
 
 
-@app.route("/game/new", methods=['GET'])
-def new_game():
-    game_id = generate_game_id()
-    print(f"{game_id}: New game")
-    game_manager.game_list[game_id] = Game(game_id, database_config, item_list)
-    return redirect(f"/game/{game_id}")
+def print_log(game_id, text):
+    current_time = datetime.now()
+    formatted_text = current_time.strftime("%Y-%m-%d %H:%M:%S.%f")[:23] + " | " + game_id + " | " + text
+    print(formatted_text)
 
 
-@app.route("/game/<game_id>", methods=['GET'])
+@app.route("/game/<game_id>")
 def game(game_id):
-    if game_id not in game_manager.game_list:
-        print(f"{game_id}: Game not found")
+    if game_id not in game_list:
+        print_log(game_id, "[fail] game: game not found")
         webpage = "<h1>Game not found</h1>"
-        return webpage
-
-    webpage = "<h1>Welcome to the game</h1>" + "<br>" + f"game_id: {game_id}"  # TODO fake response
+    else:
+        print_log(game_id, "Game started")
+        webpage = "<h1>Welcome to the game</h1>" + "<br>" + f"game_id: {game_id}"  # TODO fake response
     return webpage
 
 
-@app.route("/game/<game_id>/fetchAll", methods=['GET'])
-def fetch(game_id):
-    if game_id not in game_manager.game_list:
-        print(f"{game_id}: Game not found")
-        return {"success": False, "message": "Game not found"}
-
-    print(f"{game_id}: Fetch game data")
-    response_player = game_manager.game_list[game_id].player.get_state()
-    response_airports = game_manager.game_list[game_id].plane.airports
-    response = {"player": response_player, "airports": response_airports}
+@app.route("/game/<game_id>/get-all-data")
+def get_all_data(game_id):
+    if game_id not in game_list:
+        message = "[fail] get-all-data: game not found"
+        print_log(game_id, message)
+        response = {"success": False,
+                    "message": message}
+    else:
+        message = "[ok] get-all-data"
+        print_log(game_id, message)
+        response_player = game_list[game_id].player.get_all_data()
+        response_airports = game_list[game_id].plane.get_all_airports()
+        response = {"success": True,
+                    "message": message,
+                    "player": response_player,
+                    "airports": response_airports}
     return response
 
 
-@app.route("/game/<game_id>/buy", methods=['POST'])
+@app.route("/game/<game_id>/get-airports-data")
+def get_airports_data(game_id):
+    if game_id not in game_list:
+        message = "[fail] get-airports-data: game not found"
+        print_log(game_id, message)
+        response = {"success": False,
+                    "message": message}
+    else:
+        message = "[ok] get-airports-data"
+        print_log(game_id, message)
+        response = {"success": True,
+                    "message": message,
+                    "airports": game_list[game_id].plane.get_all_airports()}
+    return response
+
+
+@app.route("/game/<game_id>/get-player-data")
+def get_player_data(game_id):
+    if game_id not in game_list:
+        message = "[fail] get-player-data: game not found"
+        print_log(game_id, message)
+        response = {"success": False,
+                    "message": message}
+    else:
+        message = "[ok] get-player-data"
+        print_log(game_id, message)
+        response = {"success": True,
+                    "message": message,
+                    "player": game_list[game_id].player.get_all_data()}
+    return response
+
+
+@app.route("/game/new-game", methods=["POST"])
+def new_game():
+    # get player's name
+    data = request.get_json()
+    player_name = data["name"]
+
+    # generate new random game_id
+    game_id = generate_game_id()
+
+    # re-generate if game_id already exists
+    while game_id in game_list:
+        print_log(game_id, "[wtf] What? game_id already exists, re-generating")
+        game_id = generate_game_id()
+
+    # create new game
+    message = "[ok] creating new game"
+    print_log(game_id, message)
+    game_list[game_id] = Game(game_id=game_id,
+                              database_param=database_param,
+                              player_name=player_name,
+                              shop_param=shop_param,
+                              player_param=player_param,
+                              plane_param=plane_param)
+    response = {"success": True,
+                "message": message,
+                "gameID": game_id}
+    return response
+
+
+@app.route("/game/<game_id>/buy", methods=["POST"])
 def buy(game_id):
-    if game_id not in game_manager.game_list:
-        print(f"{game_id}: Game not found")
-        return {"success": False, "message": "Game not found"}
+    if game_id not in game_list:
+        message = "[fail] buy: game not found"
+        print_log(game_id, message)
+        response = {"success": False,
+                    "reason": "game",
+                    "message": message}
+    else:
+        data = request.get_json()
+        item = data["item"]
+        amount = data["amount"]
+        if item == "fuel":
+            response = game_list[game_id].shop.buy_fuel(amount)
+            print_log(game_id, response["message"])
+        elif item == "airport":
+            response = game_list[game_id].shop.buy_airport()
+            print_log(game_id, response["message"])
+        else:
+            response = game_list[game_id].shop.buy_item(item, amount)
 
-    data = request.get_json()
-    print(f"{game_id}: buy {data}")
-    return {"success": True, "money": 100, "fuel": 100}  # TODO fake response
+            print_log(game_id, response["message"])
 
-
-@app.route("/game/<game_id>/fetchAirports", methods=['GET'])
-def airports(game_id):
-    if game_id not in game_manager.game_list:
-        print(f"{game_id}: Game not found")
-        return {"success": False, "message": "Game not found"}
-
-    print(f"{game_id}: Fetch game data")
-    response = game_manager.game_list[game_id].plane.get_airports_accessibility()
     return response
 
 
-@app.route("/game/<game_id>/fly", methods=['POST'])
+@app.route("/game/<game_id>/fly", methods=["POST"])
 def fly(game_id):
-    if game_id not in game_manager.game_list:
-        print(f"{game_id}: Game not found")
-        return {"success": False, "message": "Game not found"}
+    if game_id not in game_list:
+        message = "[fail] fly: game not found"
+        print_log(game_id, message)
+        response = {"success": False,
+                    "message": message}
+    else:
+        data = request.get_json()
+        response = game_list[game_id].plane.fly(ident=data["ident"])
+        print_log(game_id, response["message"])
+    return response
 
-    data = request.get_json()
-    print(f"{game_id}: fly {data}")
-    return {"success": True, "money": 100, "fuel": 100, "emission": 100, "time": 100,
-            "location": "BBBB"}  # TODO fake response
+
+@app.route("/game/<game_id>/unload", methods=["POST"])
+def unload(game_id):
+    if game_id not in game_list:
+        message = "[fail] unload: game not found"
+        print_log(game_id, message)
+        response = {"success": False,
+                    "message": message}
+    else:
+        data = request.get_json()
+        option = data["option"]
+        response = game_list[game_id].plane.unload(option)
+        print_log(game_id, response["message"])
+    return response
 
 
 if __name__ == '__main__':
-    database_config = {"host": "127.0.0.1",
-                       "user": "root",
-                       "password": "metro",
-                       "database": "cargo_carrier",
-                       "port": 3306}
+    game_list = {}
 
-    item_list = {"fuel": {"price": 100, "description": "Fuel"},
-                 "coffee": {"price": 1, "description": "Coffee"},
-                 "airport": {"price": 1000, "description": "Airport"}}
-    game_manager = GameManager()
+    database_param = {"host": "127.0.0.1",
+                      "user": "root",
+                      "password": "metro",
+                      "database": "cargo_carrier",
+                      "port": 3306}
+
+    shop_param = {"fuel": {"price": 100, "description": "Fuel"},
+                  "coffee": {"price": 1, "description": "Coffee"},
+                  "airport": {"price": 1000, "description": "Airport"}}
+
+    player_param = {"money": 100000,
+                    "fuel": 100000,
+                    "time": 800}
+
+    plane_param = {"fuel_per_km": 0.1,
+                   "speed_per_h": 1.2,
+                   "emission_per_km": 1.2,
+                   "reward_per_km": 2,
+                   "hire_cost": 600}
     app.run(debug=True, host="127.0.0.1", port=5000)
